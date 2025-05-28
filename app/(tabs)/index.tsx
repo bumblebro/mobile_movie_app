a1417246d3ffc4c4abaf390f47670c6395f28296
Shreyas M S <shreyasmadappady20@gmail.com>
2025-05-28 23:22:10 +0530

    c

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,10 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
-import "./global.css";
+import "../global.css";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
+    <View className="items-center justify-center flex-1">
       <Text className="text-5xl font-bold text-dark-100">Welcome!</Text>
-      <Link href={"/onboarding"}>onboarding</Link>
     </View>
   );
 }
