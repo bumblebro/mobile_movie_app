cc055fdf02102aa9544abbf07427052f364c144c
Shreyas M S <shreyasmadappady20@gmail.com>
2025-05-29 12:43:39 +0530

    c

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,8 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
 import React from "react";
+import { Image, ImageBackground, Text } from "react-native";
 
 const _layout = () => {
   return (
@@ -8,6 +11,43 @@ const _layout = () => {
         name="index"
         options={{
           title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <ImageBackground
+                source={images.highlight}
+                className="w-[100px] flex justify-center items-center"
+              >
+                {" "}
+                <Image
+                  source={icons.home}
+                  tintColor="#151312"
+                  className="size-5"
+                />
+                <Text>Home</Text>
+              </ImageBackground>
+            </>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
           headerShown: false,
         }}
       />
